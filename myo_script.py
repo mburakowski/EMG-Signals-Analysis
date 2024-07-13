4a178016ecbb38c389985fd9952016f00beb7c8c
mburakowski <73072665+mburakowski@users.noreply.github.com>
2024-07-13 23:25:15 +0200

    Exporting data to .mat file

--- myo_script.py
+++ myo_script.py
@@ -1,7 +1,15 @@
 import myo
 import time
+from scipy.io import savemat
 
 class MyoListener(myo.DeviceListener):
+    def __init__(self):
+        self.data = {
+            'poses': [],
+            'orientations': [],
+            'emg': []
+        }
+
     def on_paired(self, event):
         print("Myo is paired!")
 
@@ -10,14 +18,21 @@ class MyoListener(myo.DeviceListener):
 
     def on_pose(self, event):
         print(f"Pose detected: {event.pose}")
+        self.data['poses'].append(event.pose.value)
 
     def on_orientation(self, event):
         orientation = event.orientation
-        #print(f"Orientation: {orientation}")
+        print(f"Orientation: {orientation}")
+        self.data['orientations'].append((orientation.x, orientation.y, orientation.z, orientation.w))
 
     def on_emg(self, event):
         emg = event.emg
         print(f"EMG data: {emg}")
+        self.data['emg'].append(emg)
+
+    def save_data(self, filename='myo_data.mat'):
+        savemat(filename, self.data)
+        print(f"Data saved to {filename}")
 
 def main():
     myo.init()
@@ -32,6 +47,7 @@ def main():
     finally:
         print("Shutting down Myo Hub")
         hub.shutdown()
+        listener.save_data()
 
 if __name__ == '__main__':
     main()
